11f52164368f61ea2ed191304654a578070452ff
yinong0313 <yinong0313@hotmail.com>
2022-11-15 12:59:16 -0500

    3rd update

--- RH_q1.py
+++ RH_q1.py
@@ -8,3 +8,6 @@ df1 = pd.read_csv('../Study/delta_price_symbols_A_to_D.csv',index_col=0)
 df2 = pd.read_csv('../Study/delta_price_symbols_E_to_H.csv',index_col=0)
 df3 = pd.read_csv('../Study/delta_price_symbol_X.csv',index_col=0)
 df3.head()
+
+df = df1.merge(df2, how='inner',on='Date').merge(df3,how='inner',on='Date')
+df.head(10)
