7dcb1fd63e8b0bb93c2e3f7bf731273aa98bf2df
yinong0313 <yinong0313@hotmail.com>
2022-11-15 12:09:21 -0500

    2nd update

--- RH_q1.py
+++ RH_q1.py
@@ -2,6 +2,8 @@ import pandas as pd
 import warnings
 warnings.filterwarnings('ignore')
 
+#github key: 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
+
 df1 = pd.read_csv('../Study/delta_price_symbols_A_to_D.csv',index_col=0)
 df2 = pd.read_csv('../Study/delta_price_symbols_E_to_H.csv',index_col=0)
 df3 = pd.read_csv('../Study/delta_price_symbol_X.csv',index_col=0)
